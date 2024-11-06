65ed100c6f163a5644032ce2717e18b97463c6b5
Iuhasz P. Fiona <fio.iuhasz@gmail.com>
2024-11-06 09:31:43 +0200

    React Router Dom install

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-red-600 text-3xl flex justify-center items-center font-bold">
+      <h1 className="text-red-600 text-3xl flex justify-center items-center font-bold shadow-xl shadow-black">
         PrimePixel
       </h1>
     </>
