5bb8b058bde00ddf708c89a4aa823fe77315caf3
Iuhasz P. Fiona <fio.iuhasz@gmail.com>
2024-11-06 09:23:47 +0200

    Project init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600 text-3xl flex justify-center items-center font-bold">
+        PrimePixel
+      </h1>
+    </>
+  );
+}
+
+export default App;
